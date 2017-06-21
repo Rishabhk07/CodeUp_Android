c0f2dbe68a7b5763f0b6fdda15a426fbdd0de851
Naman Dwivedi <namandwivedi14@gmail.com>
2017-06-21 14:55:10 +0530

    show questions

--- app/src/main/java/com/codingblocks/codeup/CodeupActivity.java
+++ app/src/main/java/com/codingblocks/codeup/CodeupActivity.java
@@ -3,6 +3,20 @@ package com.codingblocks.codeup;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by naman on 21/6/17.
@@ -10,9 +24,56 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CodeupActivity extends AppCompatActivity {
 
+    Editor editor;
+    TextView tvTitle;
+    View codelayout;
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_codeup);
+
+        editor = (Editor) findViewById(R.id.editor);
+        tvTitle = (TextView) findViewById(R.id.tv_question_title);
+        codelayout = findViewById(R.id.code_layout);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        fetchQuestions();
+    }
+
+    private void fetchQuestions(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("questions");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterator<DataSnapshot> iterator = dataSnapshot.getChildren().iterator();
+                List<Question> questions =new ArrayList<>();
+                while (iterator.hasNext()){
+                    DataSnapshot snapshot = iterator.next();
+                    Question question = snapshot.getValue(Question.class);
+                    questions.add(question);
+                }
+
+                showQuestion(questions.get(0));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("lol",databaseError.getDetails());
+            }
+        });
+    }
+
+    private void showQuestion(Question question) {
+        progressBar.setVisibility(View.GONE);
+        codelayout.setVisibility(View.VISIBLE);
+
+        tvTitle.setText(question.getStatement());
+
+        if (question.getStub()!=null){
+            editor.setText(question.getStub());
+        }
     }
 }
---
+++ app/src/main/java/com/codingblocks/codeup/Question.java
@@ -0,0 +1,29 @@
+package com.codingblocks.codeup;
+
+/**
+ * Created by naman on 21/6/17.
+ */
+
+public class Question {
+
+    private String statement;
+    private String input;
+    private String output;
+    private String stub;
+
+    public String getStub() {
+        return stub;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+}
