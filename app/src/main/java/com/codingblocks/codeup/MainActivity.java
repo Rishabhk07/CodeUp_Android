c67b9c656b6f60c6eb247656c5e24717712cdf76
Arnav Gupta <arnav@codingblocks.com>
2017-06-21 12:17:32 +0530

    initial commit
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ app/src/main/java/com/codingblocks/codeup/MainActivity.java
@@ -0,0 +1,13 @@
+package com.codingblocks.codeup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
